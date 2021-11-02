6dcfa367bb845fe8b1bc5a6ae260dcb4955fdba6
nadimsheikh07 <nadim.sheikh.07@gmail.com>
2021-11-02 23:06:54 +0530

    add pdf

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use niklasravnsborg\LaravelPdf\Facades\Pdf;
+
+class HomeController extends Controller
+{
+    public function index()
+    {
+        $data = [
+            'foo' => 'bar'
+        ];
+
+        $pdf = Pdf::loadView('welcome', $data);
+        $pdf->save('document.pdf');
+        // return $pdf->stream('document.pdf');
+        // return $pdf->download('RefillUnitReceipt.pdf');
+        return view('welcome');
+    }
+}
--- config/app.php
+++ config/app.php
@@ -174,7 +174,7 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-
+        niklasravnsborg\LaravelPdf\PdfServiceProvider::class
     ],
 
     /*
@@ -227,8 +227,7 @@ return [
         'Str' => Illuminate\Support\Str::class,
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
-        'View' => Illuminate\Support\Facades\View::class,
-
+        'View' => Illuminate\Support\Facades\View::class,        
     ],
 
 ];
